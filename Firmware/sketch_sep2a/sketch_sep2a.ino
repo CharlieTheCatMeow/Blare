345401cd349377fa7d3d8775389f24c6b5403ff7
Xuan Zhu <charliecheesycatmeow@gmail.com>
2026-09-02 15:23:54 +0200

    Firmware and Gerbers

---
+++ Firmware/sketch_sep2a/sketch_sep2a.ino
@@ -0,0 +1,33 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+#include <SPI.h>
+
+#define TFT_SCLK 9
+#define TFT_MOSI 10
+#define TFT_RST 8
+#define TFT_DC 4
+#define TFT_CS 5
+#define TFT_BL 6
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+void setup() {
+  Serial.begin(115200)
+
+  tft.init(76, 284); // Our panel size (portrait)
+  tft.setOffsets(82, 18); // Offsets for the weird resolution
+  tft.invertDisplay(false); // Invert the colors (This display is flipped from normal)
+  tft.setRotation(1); // Landscape, if it's upside down use 3!
+  tft.fillScreen(ST77XX_BLACK); // clear the screen
+  Serial.println("TFT Initialized!");
+  tft.setCursor(0,0); // make the cursor at the top left
+
+  tft.setTextSize(6);
+
+  tft.print("E");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
