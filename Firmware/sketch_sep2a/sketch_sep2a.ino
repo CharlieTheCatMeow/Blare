0252821fc0f61cce57a93164016aebbc0d48e3eb
Xuan Zhu <charliecheesycatmeow@gmail.com>
2026-09-08 18:25:04 +0200

    Firmware update test switches

--- Firmware/sketch_sep2a/sketch_sep2a.ino
+++ Firmware/sketch_sep2a/sketch_sep2a.ino
@@ -9,10 +9,20 @@
 #define TFT_CS 5
 #define TFT_BL 6
 
+#define SWITCH_PIN_1 0
+#define SWITCH_PIN_2 1
+#define SWITCH_PIN_3 2
+#define SWITCH_PIN_4 3
+
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 void setup() {
   Serial.begin(115200)
+
+  pinMode(SWITCH_PIN_1, INPUT_PULLUP);
+  pinMode(SWITCH_PIN_2, INPUT_PULLUP);
+  pinMode(SWITCH_PIN_3, INPUT_PULLUP);
+  pinMode(SWITCH_PIN_4, INPUT_PULLUP);
 
   tft.init(76, 284); // Our panel size (portrait)
   tft.setOffsets(82, 18); // Offsets for the weird resolution
@@ -28,6 +38,33 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int switch1State = digitalRead(SWITCH_PIN_1);
+  int switch2State = digitalRead(SWITCH_PIN_2);
+  int switch3State = digitalRead(SWITCH_PIN_3);
+  int switch4State = digitalRead(SWITCH_PIN_4);
 
+  if (switch1State == LOW) {
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0)
+    tft.print("Switch 1 works")
+    delay(200);
+  }
+  else if (switch1State == LOW) {
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0)
+    tft.print("Switch 2 works")
+    delay(200);
+  }
+  else if (switch1State == LOW) {
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0)
+    tft.print("Switch 3 works")
+    delay(200);
+  }
+  else if (switch1State == LOW) {
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0)
+    tft.print("Switch 4 works")
+    delay(200);
+  }
 }
